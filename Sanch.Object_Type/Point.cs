13420a4d8c3f6a7f69613bbd2136a651fab83f72
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 19:49:55 +0800

    Добавьте файлы проекта.

---
+++ Sanch.Object_Type/Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Object_Type
+{
+    class Point
+    {
+        public int X { get; set; }
+        //можно сравнивать не по памяти (смотри ниже)
+        public Point Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //никогда нельзя делать(реализовывать) exseption (всегда либо тру либо фолс)
+            if (obj == null) //проверка на null
+            {
+                return false;
+            }
+            //is as / is - возвращает булевое значение если этот тип является тем же типом с которым мы сравниваем
+            //as - является типом который мы указали то переменная приводится в этот тип, иначе null
+
+            if (obj is Point point) // var point = obj as Point;
+            {
+                //сравнение которое нам нужно
+                return point.X == X; //сравниваем не по памяти а по значению
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode() //Equals на минималках (должа быть быстрой)
+        {
+            return X; //быстро возвращает значение
+        }
+        public override string ToString() //приведение к строковому значению (число в формате строки)
+        {
+            return X.ToString();
+        }
+
+        public new Type GetType() //можем преопределить метод с помощью "new" - не надо так делать (но такое возможно)
+        {
+            return typeof(UInt16);
+        }
+        
+        public Point Clone()
+        {
+            return MemberwiseClone() as Point;//-неглубокое копирование (продублируются значимые переменные и они разные (например X))
+            // если есть ссылочные переменные, то они будут одни и те же таи и там      
+        }
+
+
+    }
+}
---
+++ Sanch.Object_Type/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bb = System.Int32;
+
+namespace Sanch.Object_Type
+{
+    class Program //: System.Object - не надо (неявное наследование)
+    {
+        static void Main(string[] args)
+        {
+            //обджект - базовый класс (тип)
+            //есть методы которые помогают правильно работать
+
+            //System.Object == object
+            //System.Int64 === int
+
+            object obj = new object();
+            //obj.Equals - позволяет выполнять сравнение (если значимые то сравниваем по значению
+            //если ссылочные - то сравниваем ссылки(адреса)
+
+            int i = 5;
+            int j = 5;
+            Console.WriteLine(i.Equals(j));// - сравниваем значения 
+
+            object oi = (object)i;//упаковка (boxing)
+            object oj = (object)j;//упаковка (boxing)
+
+            Console.WriteLine(oi.Equals(oj)); //сравнение ссылочных
+
+            var p1 = new Point() { X = 5 };
+            var p2 = new Point() { X = 5 };
+
+            Console.WriteLine(p1.Equals(p2)); //сравнение ссылочных ( но мы переделали на сравнение по значению)
+
+
+            Console.WriteLine();
+            Console.WriteLine(i.GetHashCode());//число
+            Console.WriteLine(oi.GetHashCode());//число
+            Console.WriteLine(new MyClass().GetHashCode());//адрес памяти
+            Console.WriteLine(p1.GetHashCode());//число которое мы преопределили
+
+            Console.WriteLine();
+            Console.WriteLine(i.ToString()); //число в строковом формате
+            Console.WriteLine(p1.ToString()); //преопределили строковое значение
+
+            Console.WriteLine();
+            Console.WriteLine(i.GetType()); // будет возвращен тип
+            Console.WriteLine(oi.GetType()); //тип упакованный
+            Console.WriteLine(p1.GetType()); //наш тип
+            Console.WriteLine(typeof(Point) == p1.GetType());  //typeof(class) - получаем тип класса
+
+            Console.WriteLine();
+            //object.ReferenceEquals - сравниваем объекты по ссылке - игнор значения
+            Console.WriteLine(Object.Equals(5, 5));
+            Console.WriteLine(Object.ReferenceEquals(5, 5)); //false - потому что две разные переменные в памяти ( разные ссылки) 
+            Console.WriteLine(Object.ReferenceEquals(p2, p2)); //один и тот же участок памяти
+
+            Console.WriteLine();
+            //MemberwiseClone() - позволяет создавать дубликаты объектов (используется только внутри класса)
+            //клоны - дополнительный скопированный объект который будет содержать все те же поля которые 
+            //содержались в исходном, но это будет поддельный другой объект 
+
+            var pp = new Point() { X = 7, Y = new Point() };
+            var pp2 = pp; //участок памяти один и тот же (будут изменять все если изменить что то в другом) 
+            // а если сделать дубликат - то они будут независимы друг от друга
+            pp2.X= 77; // поменяется и у pp
+            pp2.Y = new Point
+            {
+                X = 99
+            };
+            Console.WriteLine(pp);
+            Console.WriteLine(pp.Y);
+            var pp3 = pp.Clone();
+            pp3.X = 88; //поменяется только pp3
+            pp3.Y.X = 222; //поменялось и у pp так как это ссылочный тип
+            
+            Console.WriteLine(pp.Y);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
